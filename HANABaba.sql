e186c880b9eb3d5a0653fe5cf5a231f385791153
Nabheet Madan <Nabheet.cloud@gmail.com>
2018-05-03 23:40:32 +0530

    Hana baba

---
+++ HANABaba.sql
@@ -0,0 +1,11 @@
+/*
+--- Create Schema
+create schema myfirstGCPSchema
+---Grant Select
+GRANT SELECT on SCHEMA myFirstGCPSchema to Devuser
+
+grant select,create any on schema myfirstgcpschema to devuser
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON SCHEMA myfirstgcpschema to _SYS_REPO WITH GRANT OPTION
+*/
+
